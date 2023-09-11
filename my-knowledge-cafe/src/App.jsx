c5272bf7a0329fc296b2811fbf7ea76952ad0e62
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2023-09-11 23:51:02 +0600

    first commit

---
+++ my-knowledge-cafe/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   
+ <h1 className='text-center text-6xl text-yellow-500'> ami mahim</h1>
+     
+    </>
+  )
+}
+
+export default App
